4038ae3070038d3822a7380e8dd80d561eeb070c
hectorcrdna <hectorcrdna@gmail.com>
2026-08-26 11:53:05 -0400

    Added computed property habits and data controller to ContentView.

--- HabitsTracker/ContentView.swift
+++ HabitsTracker/ContentView.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var habits: [Habit] {
+        let filter = dataController.selectedFilter ?? .all
+        var allHabits: [Habit]
+        
+        if let tag = filter.tag {
+            allHabits = tag.habits?.allObjects as? [Habit] ?? []
+        } else {
+            let request = Habit.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allHabits = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        
+        return allHabits.sorted()
+    }
+    
     var body: some View {
-        Text("Content")
+        List {
+            ForEach(habits) { habit in
+                Text(habit.habitTitle)
+            }
+        }
+        .navigationTitle("Habits")
     }
 }
 
