55bb58fda8fc4b6d3027b3f3e2f27fb087a781bc
hectorcrdna <hectorcrdna@gmail.com>
2026-09-06 02:59:52 -0400

    Added test for performance.

---
+++ HabitsTrackerTests/PerformanceTests.swift
@@ -0,0 +1,30 @@
+//
+//  PerformanceTests.swift
+//  HabitsTrackerTests
+//
+//  Created by Hector Cardona on 9/6/26.
+//
+
+import CoreData
+import XCTest
+@testable import HabitsTracker
+
+final class PerformanceTests: BaseTestCase {
+	func testAwardsCalculationPerformance() {
+		for _ in 1...100 {
+			dataController.createSampleData()
+		}
+
+		let awards = Array(repeating: Award.allAwards, count: 25).joined()
+
+		XCTAssertEqual(
+			awards.count,
+			500,
+			"This checks that the array of awards is built correctly, change if you add awards."
+		)
+
+		measure {
+			_ = awards.filter(dataController.hasEarned)
+		}
+	}
+}
